37a612a07e4c33440469345abacdb77f91d8dab0
Bede Jakab <jakab.bede@stud.uni-corvinus.hu>
2022-12-09 12:15:26 +0100

    Csatlakozás adatbázishoz

---
+++ gyak11/Controllers/BoatController.cs
@@ -0,0 +1,24 @@
+﻿using gyak11.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gyak11.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+
+
+    }
+
+}
